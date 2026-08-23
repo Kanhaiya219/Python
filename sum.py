f89f9054674f573569328b7adb22529da7fb1aaa
Kanhaiya <krishankanhaiya28012003@gmail.com>
2026-08-23 19:44:59 +0530

    Fix sum program formatting

--- sum.py
+++ sum.py
@@ -1,6 +1,5 @@
 #program to sum 2 digits
-
 first = int(input("enter first number : "))
-second = int(input("enter second nummber : "))
+second = int(input("enter second number : "))
 
-print("sum of ",first,"and ",second,"is : ", first+second)
+print("sum of ", first, "and", second, "is :", first + second)
