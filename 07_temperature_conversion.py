d9f4eafe0feb33956370a5fc62ea3d4a1d153abc
Kanhaiya <krishankanhaiya28012003@gmail.com>
2026-08-26 16:59:39 +0530

    Changes Temprature From Celsius To Ferhanite

---
+++ 07_temperature_conversion.py
@@ -0,0 +1,8 @@
+#Convert Celsius to Fahrenheit
+#formula °F = (°C × 9/5) + 32
+
+celsius = float(input("Enter temperature in Celsius: "))
+
+fahrenheit = (celsius * 9 / 5) + 32
+
+print(celsius, "°C in Fahrenheit is:", fahrenheit, "°F")
