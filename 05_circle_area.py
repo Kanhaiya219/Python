132da9f7a6ed0e71b3d4b6499f432b28eb18d3ce
Kanhaiya <krishankanhaiya28012003@gmail.com>
2026-08-26 16:45:12 +0530

    Finds Area Of Circle

---
+++ 05_circle_area.py
@@ -0,0 +1,18 @@
+#Program To Find Area Of Circle
+
+radius = float(input("Enter radius: "))
+
+area = 3.14 * radius * radius
+
+print("Area of circle is:", area)
+
+
+#With math library
+
+"""import math
+
+radius = float(input("Enter radius: "))
+
+area = math.pi * radius ** 2
+
+print("Area of circle is:", area)"""
