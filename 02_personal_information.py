e6258e237598dec2edcc03f91300fc6616fe2d64
Kanhaiya <krishankanhaiya28012003@gmail.com>
2026-08-26 16:18:29 +0530

    Prints Your Name , Age and City

---
+++ 02_personal_information.py
@@ -0,0 +1,8 @@
+#Display your name, age, and city
+
+name = input("Enter your name : ")
+age = input("Enter your age : ")
+city = input("Enter your city : ")
+
+print("\nWelcome",name)
+print("Name :",name,"\nAge :",age,"\nCity :",city)
