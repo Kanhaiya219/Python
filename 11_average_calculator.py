8566bd50ce20172f7db03bdb3808e443f997deab
Kanhaiya <krishankanhaiya28012003@gmail.com>
2026-08-26 18:01:01 +0530

    Calculate average of three numbers

---
+++ 11_average_calculator.py
@@ -0,0 +1,11 @@
+#Calculates Average of 3 numbers
+num_1 = int(input("Enter First Number : "))
+num_2 = int(input("Enter Second Number : "))
+num_3 = int(input("Enter Third Number : "))
+
+average = (num_1 + num_2 + num_3 ) / 3
+
+print("\nFirst Number : ",num_1)
+print("Second Number : ",num_2)
+print("Third Number : ",num_3)
+print("\nAverage : ",average)
