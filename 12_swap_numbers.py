e5ea7a7641806144ab95d3bbdddb4dd744577f1f
Kanhaiya <krishankanhaiya28012003@gmail.com>
2026-08-26 18:11:28 +0530

    Swap 2 Number With And Without Using Third Variable

---
+++ 12_swap_numbers.py
@@ -0,0 +1,39 @@
+#Swap Two Numbers
+
+"""
+a = int(input("Enter First Number : "))
+b = int(input("Enter Second Number : "))
+c = 0
+
+print("Before Swap\n")
+print("\tA :",a)
+print("\tB :",b)
+
+
+c = a
+a = b
+b = c
+
+print("After Swap\n")
+print("\tA :",a)
+print("\tB :",b)
+
+"""
+
+
+#Swap Two Numbers Without Using Third Varable
+
+a = int(input("Enter First Number : "))
+b = int(input("Enter Second Number : "))
+
+
+print("Before Swap\n")
+print("\tA :",a)
+print("\tB :",b)
+
+a,b = b,a
+
+
+print("After Swap\n")
+print("\tA :",a)
+print("\tB :",b)
