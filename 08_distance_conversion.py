c0bce65102fc8d5a775e77f4cf5412364bf96901
Kanhaiya <krishankanhaiya28012003@gmail.com>
2026-08-26 17:08:31 +0530

    Converts kilometers to miles

---
+++ 08_distance_conversion.py
@@ -0,0 +1,15 @@
+#Convert kilometers to miles
+
+"""
+    Kilometers → Miles
+        Miles = Kilometers X 0.621371
+
+    Miles → Kilometers
+        Kilometers = Miles X 1.60934
+"""
+
+Kilometers = float(input("Enter KiloMeters To Convert In Miles : "))
+
+Miles = Kilometers * 0.621371
+
+print(Kilometers,"Kilometers in Miles is : ",Miles)
