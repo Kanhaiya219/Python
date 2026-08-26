63312cc0588792baf3d7a9c3f8e51b5f2581b21c
Kanhaiya <krishankanhaiya28012003@gmail.com>
2026-08-26 16:39:32 +0530

    Basic Arithmetic Operations

---
+++ 04_arithmetic_operations.py
@@ -0,0 +1,15 @@
+#Performs Basic Arithmetic Operations Like Sum , Substraction , Multiplicaton , Division
+
+print("Enter Two Numbers\n............................")
+num_1 = int(input("Enter first number : "))
+num_2 = int(input("Enter second number : "))
+
+add = num_1 + num_2
+sub = num_1 - num_2
+multi = num_1 * num_2
+div = num_1 / num_2
+
+print("\nAddition of ",num_1,"and",num_2,"is : ",add)
+print("Substraction of ",num_1,"and",num_2,"is : ",sub)
+print("Multiplication of ",num_1,"and",num_2,"is : ",multi)
+print("Division of ",num_1,"and",num_2,"is : ",div)
