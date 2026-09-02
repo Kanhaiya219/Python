37a8da68ec7f2ccc3f1281fd8880299b2a68c067
Kanhaiya <krishankanhaiya28012003@gmail.com>
2026-09-02 21:51:10 +0530

    Check whether a number is positive, negative, or zero

---
+++ 16_number_sign_checker.py
@@ -0,0 +1,10 @@
+#Calculate total and percentage of marks
+
+num = int(input("Enter Any Number : "))
+
+if(num > 0 ):
+    print(f"{num} Is A Positive Number")
+elif(num < 0 ):
+    print(f"{num} Is A Negative Number")
+else :
+    print(f"{num} Is Zero")
