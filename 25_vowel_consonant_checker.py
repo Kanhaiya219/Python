9c859f960dbb0ef4cac5d4af354d0b7cdb383ad8
Kanhaiya <krishankanhaiya28012003@gmail.com>
2026-09-04 08:27:15 +0530

    Check whether a character is a vowel or consonant

---
+++ 25_vowel_consonant_checker.py
@@ -0,0 +1,9 @@
+#Check whether a character is a vowel or consonant
+
+char  = input("Enter any alphabet to check :")
+
+if( char == "a" or "e" or "i" or "o" or "u" or "A" or "E" or "I" or "O" or "U"):
+    print(f"{char} is a vowel")
+
+else :
+    print(f"{char} is a consonant")
