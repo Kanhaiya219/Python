172d5356f269f9cf35fc8abc1ce892ae193e0897
Kanhaiya <krishankanhaiya28012003@gmail.com>
2026-09-04 13:57:30 +0530

    Create a basic ATM menu system

---
+++ 30_atm_menu_system.py
@@ -0,0 +1,38 @@
+# Basic ATM Menu System
+
+balance = 5000
+
+while True:
+    print("\n===== ATM MENU =====")
+    print("1. Check Balance")
+    print("2. Deposit Money")
+    print("3. Withdraw Money")
+    print("4. Exit")
+
+    choice = int(input("Enter your choice: "))
+
+    if choice == 1:
+        print("Your balance is Rs", balance)
+
+    elif choice == 2:
+        amount = float(input("Enter amount to deposit: "))
+        balance = balance + amount
+        print("Money deposited successfully!")
+        print("Updated balance = Rs", balance)
+
+    elif choice == 3:
+        amount = float(input("Enter amount to withdraw: "))
+
+        if amount <= balance:
+            balance = balance - amount
+            print("Please collect your cash.")
+            print("Remaining balance = Rs", balance)
+        else:
+            print("Insufficient balance!")
+
+    elif choice == 4:
+        print("Thank you for using the ATM.")
+        break
+
+    else:
+        print("Invalid choice!")
